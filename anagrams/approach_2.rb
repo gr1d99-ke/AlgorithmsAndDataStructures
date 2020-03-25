e66f57194c633d2a832de3994ab5ba21391d46ac
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-03-25 11:27:51 +0300

    anagrams approach 2

---
+++ anagrams/approach_2.rb
@@ -0,0 +1,35 @@
+module Anagrams
+  module Approach2
+    def self.call(str1, str2)
+      str1 = str1.tr(" ", "")
+      str2 = str2.tr(" ", "")
+      str1.downcase!
+      str2.downcase!
+      str1_hash = {}
+      (0...str1.length).each do |i| # n iterations
+        key = str1[i]
+        if str1_hash.key?(key)
+          str1_hash[key] += 1
+        else
+          str1_hash[str1[i]] = 1
+        end
+      end
+
+      (0...str2.length).each do |i| # n iterations
+        key = str2[i]
+        if str1_hash.key?(key)
+          str1_hash[key] -= 1
+        end
+      end
+
+      str1_hash.values.all? { |v| v.zero? }
+
+      # complexity
+      # O(N) -> O(N + N) -> O(2N) -> O(N)
+    end
+  end
+end
+
+p Anagrams::Approach2.call("incest", "insect")
+p Anagrams::Approach2.call("William Shakespeare", "I am a weakish speller")
+p Anagrams::Approach2.call("Tom Marvolo Riddle", "I am Lord Voldemort")
